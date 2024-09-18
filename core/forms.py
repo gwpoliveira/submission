63d6553ac574b3e430425b2a9b555b6a027750a5
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-09-18 19:49:05 -0300

    Estilização

--- core/forms.py
+++ core/forms.py
@@ -2,16 +2,14 @@ from django import forms
 from .models import Profile, Submission, Component
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import inlineformset_factory
-from django import forms
 from django.forms import modelformset_factory
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['nome','cpf', 'whatsapp', 'comprovante_pagamento'] #alteração
+        fields = ['nome', 'cpf', 'whatsapp', 'comprovante_pagamento']
         widgets = {
-            'nome': forms.TextInput(attrs={'class': 'form-control'}), #alteração
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
             'cpf': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CPF'}),
             'whatsapp': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'WhatsApp'}),
             'comprovante_pagamento': forms.FileInput(attrs={'class': 'form-control'}),
@@ -21,18 +19,34 @@ class SubmissionForm(forms.ModelForm):
     class Meta:
         model = Submission
         fields = ['title', 'thematic_axis', 'modality', 'arquivo']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título do Trabalho'}),
+            'thematic_axis': forms.Select(attrs={'class': 'form-select'}),  # Campo de seleção
+            'modality': forms.Select(attrs={'class': 'form-select'}),       # Campo de seleção
+            'arquivo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
 
 class ComponentForm(forms.ModelForm):
     class Meta:
         model = Component
         fields = ['nome', 'cpf', 'email']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
+            'cpf': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CPF'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+        }
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)  # Certifique-se de que o email é obrigatório
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))  # Certifique-se de que o email é obrigatório
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
 
 # Formset para múltiplos componentes
 ComponentFormSet = modelformset_factory(Component, form=ComponentForm, extra=1)
